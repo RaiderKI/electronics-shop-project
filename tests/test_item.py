4b293229db8cb06c1d8ed7793cb22280505c525b
Ivan Korekov <Vanek77721@gmail.com>
2023-06-22 00:31:05 +0500

    initial

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("мышка", 50, 5)
+    assert item.calculate_total_price() == 250
+
+
+def test_apply_discount():
+    item = Item("Клавиатура", 75, 5)
+    item.pay_rate = 1.6
+    item.apply_discount()
+    assert item.price == 120
+
